54959f2cf708610067587b4e83ab957593917864
Timo Mämecke <timo@maemecke.com>
2022-07-09 00:38:00 +0200

    Fix missing prop types

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import 'focus-visible'
 import '@/styles/tailwind.css'
+import { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
